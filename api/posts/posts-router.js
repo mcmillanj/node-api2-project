cbe3d42cd4aa1b428cae96361b9a188163fab9d8
therman mcmillan <mcmillanj0106@yahoo.com>
2021-11-30 20:04:17 -0500

    14 done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -58,23 +58,42 @@ router.post('/', (req, res) => {
     }
   })
 
+
   router.put('/:id', (req, res) => {
-    const changes = req.body
-    Post.update(req.params.id, changes)
-    .then(post => {
-        if(post) {
-            res.status(200).json(changes);
-        } else {
-            res.status(404).json({ message: 'The post with the specified ID does not exist' })
+const {title,contents}= req.body
+    if(!title || !contents) {
+        res.status(400).json({message:'please provide title and contents for the post '
+    })
+    }else {
+        Post.findById(req.params.id)
+        .then( ans => {
+        if(!ans){     
+      res.status(404).json({
+          message:'the post with the specified ID does not exit',
+        })
+        } else {        
+                    
+        return Post.update(req.params.id, req.body)
         }
     })
-    .catch(error => {
-        console.log(error);
+    .then(data => {
+        if (data){
+            return Post.findById(req.params.id)
+        }
+       })  
+       .then(post => {
+           if(post){
+          res.json(post)
+           }
+       })   
+        .catch(err => {       
         res.status(500).json({
-            message: "The post information could not be modified",
+            message: "The post information could not be retrieved",
+            err:err.message
              })
     
     })
+}
 })
 
 router.delete('/:id',async (req, res) => {
@@ -91,6 +110,7 @@ router.delete('/:id',async (req, res) => {
         
         res.status(500).json({
             message: 'Error removing the post',
+            err: err.message
         })
     }
 })
